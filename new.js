59201d4564f52f3535b610491b1870d476d12494
Chiranjeevi sai charan <saicharan922014@gmail.com>
2024-10-01 11:38:43 +0530

    modified

---
+++ new.js
@@ -0,0 +1,83 @@
+// Task 1
+// ----------------------------------
+for(var a=1; a<=10; a++){
+    console.log(a);
+}
+var a=1;
+while(a<=10){
+    a++;
+console.log(a);
+
+}
+var a=1;
+do{
+    a++
+console.log(a);
+
+}while(a<=10);
+// task 2
+// ---------------------------------
+for(var a=1;a<=10;a++){
+  if (a%2===0){
+    console.log(a +" "+"is even no");
+  }
+
+}
+var a=1;
+while(a<=10){
+    a++;
+    if(a%2===0)
+    {
+        console.log(a +" "+"is even no");
+    }
+
+
+}
+var a=1;
+do {
+a++;
+if(a%2===0)
+        {
+            console.log(a +" "+"is even no");
+        }
+}while(a<=10);
+// task 3
+// -----------------------------------
+for(var a=1;a<=10;a++){
+  if (a%2===1){
+    console.log(a +" "+"is even no");
+  }
+
+}
+var a=1;
+while(a<=10){
+    a++;
+    if(a%2===1)
+    {
+        console.log(a +" "+"is even no");
+    }
+
+
+}
+var a=1;
+do {
+a++;
+if(a%2===1)
+        {
+            console.log(a +" "+"is even no");
+        }
+}while(a<=10);
+// // Task 4 ----------------------
+
+
+var marks="50"
+if(marks>=35 && marks<=45){
+console.log("yours marks are in btw 35 to 45");
+
+}
+else{
+    console.log("your marks are not btw 35 to 45");
+}
+
+
+
